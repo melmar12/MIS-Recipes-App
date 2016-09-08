615bf498442b81c4f853873fa4a1867b6b18341c
Melissa Martinez <melmar12@live.com>
2016-09-08 12:50:38 -0500

    did all the things

---
+++ my-recipes-sample/db/seeds.rb
@@ -0,0 +1,28 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Recipe.create(name: "tea", description: "yummyyyyyy" )
+Recipe.create(name: "cupcake", description: "yummyyyyyy" )
+Recipe.create(name: "beans", description: "yummyyyyyy" )
+Recipe.create(name: "pizza", description: "yummyyyyyy" )
+Recipe.create(name: "smoothie", description: "yummyyyyyy" )
+Recipe.create(name: "tea", description: "yummyyyyyy" )
+Recipe.create(name: "cupcake", description: "yummyyyyyy" )
+Recipe.create(name: "beans", description: "yummyyyyyy" )
+Recipe.create(name: "pizza", description: "yummyyyyyy" )
+Recipe.create(name: "smoothie", description: "yummyyyyyy" )
+Recipe.create(name: "tea", description: "yummyyyyyy" )
+Recipe.create(name: "cupcake", description: "yummyyyyyy" )
+Recipe.create(name: "beans", description: "yummyyyyyy" )
+Recipe.create(name: "pizza", description: "yummyyyyyy" )
+Recipe.create(name: "smoothie", description: "yummyyyyyy" )
+Recipe.create(name: "tea", description: "yummyyyyyy" )
+Recipe.create(name: "cupcake", description: "yummyyyyyy" )
+Recipe.create(name: "beans", description: "yummyyyyyy" )
+Recipe.create(name: "pizza", description: "yummyyyyyy" )
+Recipe.create(name: "smoothie", description: "yummyyyyyy" )
